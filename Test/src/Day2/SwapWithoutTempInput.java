2497a8c12414c1042923c161e6dea3717b3be86f
marulesh27 <maruleshm@gmail.com>
2025-07-27 14:12:36 +0530

    Second commit

---
+++ Test/src/Day2/SwapWithoutTempInput.java
@@ -0,0 +1,17 @@
+package Day2;
+
+import java.util.Scanner;
+public class SwapWithoutTempInput {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter first number: ");
+int a = sc.nextInt();
+System.out.print("Enter second number: ");
+int b = sc.nextInt();
+a = a + b;
+b = a - b;
+a = a - b;
+System.out.println("After swapping: a = " + a + ", b = " + b);
+sc.close();
+}
+}
